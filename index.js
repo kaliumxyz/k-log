d515d44a7016ec0d1862f45aac57689c201ef365
Martijn Becker <martijn@becker.wtf>
2017-05-19 22:46:37 +0200

    small update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const fs = require('fs')
 
 // Logs the current date and a string (text) to a file named log in the same dir.
-function log(text) {
+function log(text, fileName, formatting) {
+
+	filename = filename || "log"
+
+	if (!formatting)
 	text = JSON.stringify(new Date()) + " - " + text + "\n"
+
 	console.log(text)
-	const stream = fs.createWriteStream('log', { flags: 'a', defaultEncoding: 'utf8' })
+	const stream = fs.createWriteStream(filename, { flags: 'a', defaultEncoding: 'utf8' })
 	stream.write(text)
 	return true
 }
