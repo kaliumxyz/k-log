14b56e1f33cbc43eb321aaae55b5ce6d767bc088
kaliumxyz <martijn@becker.wtf>
2017-05-21 20:50:33 +0200

    actually made class usefull

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ const fs = require('fs')
 
 // Logs the current date and a string (text) to a file named log in the same dir.
 class logger{
-	constructor(text, filename = 'log.log', formatting = true, print = true) {
-
-		if (formatting)
+	constructor(filename = 'log.log', formatting = true, print = true) {
+		this.stream = fs.createWriteStream(filename, { flags: 'a', defaultEncoding: 'utf8' })
+		this.formatting = formatting
+		this.filename = filename
+		this.print = print
+	}
+	log(text) {
+		if (this.formatting)
 			text = JSON.stringify(new Date()) + " - " + text + "\n"
-		if (print)
+		if (this.print)
 			console.log(text)
-		const stream = fs.createWriteStream(filename, { flags: 'a', defaultEncoding: 'utf8' })
-		stream.write(text)
-		
-}
+		this.stream.write(text)
+
+	}
 }
 
 // legacy support
